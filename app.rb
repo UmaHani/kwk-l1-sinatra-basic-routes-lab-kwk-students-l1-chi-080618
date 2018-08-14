73ba0b7914f0b9f99e45c1d61b575ef5983f3591
UmaHani <umahanimohammadi@yahoo.com>
2018-08-14 16:52:31 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,10 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do 
+    "My name is Uma"
+    get '/hometown' do
+      "My hometown is Hyderabad"
+      get '/favorite-song' do
+      "My favorite song is Twinkle Twinkle Little Star"
 end
